44a1481024318e4397e6d363f7d03d28e1fabc6f
DilmyPerera <samadiniperera20@gmail.com>
2024-12-25 19:26:37 +0530

    Import and setup PaperProvider, Appbar, and basic React Native components

--- CurrencyConverter/components/Home.js
+++ CurrencyConverter/components/Home.js
@@ -1,3 +1,12 @@
+import React, { useState } from 'react';
+import { SafeAreaView, Text, TextInput, Button, StyleSheet, View } from 'react-native';
+import { Provider as PaperProvider, Appbar } from 'react-native-paper';
+
 export default function Home() {
-  return <View></View>;
+  return(
+<View>
+    
+
+  </View>;
+  ) ;
 }
