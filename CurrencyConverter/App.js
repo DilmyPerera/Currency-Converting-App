c0fe057fb70a391ed91d79e6f5220a7cb1de3f4e
DilmyPerera <samadiniperera20@gmail.com>
2024-12-26 08:30:55 +0530

    Update SafeAreaView with inline flex styling

--- CurrencyConverter/App.js
+++ CurrencyConverter/App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native";
+import { PaperProvider } from "react-native-paper";
 import Home from "./components/Home";
 
 export default function App() {
   return (
-    <View>
-      <Home />
-    </View>
+    <PaperProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Home />
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
